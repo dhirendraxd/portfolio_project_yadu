2775a4e1aa378bffbd1d65439d194df907d957e1
Dhiren <dhirendraxd@gmail.com>
2025-10-19 20:01:18 +0545

    refactor: remove unused color configurations from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,3 @@ import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
 
-                    foreground: 'hsl(var(--coral-foreground))' 
-                                                    },
-                                                                    'sky-blue': {
-                                                                        background: 'hsl(var(--sky-blue-background))',
-                                                                        foreground: 'hsl(var(--sky-blue-foreground))'
-                                                                    },
-                                                                                    stone: {
-                                                                        background: 'hsl(var(--stone-background))',
-                                                                        foreground: 'hsl(var(--stone-foreground))'
-                                                                    },
-                                                                    
